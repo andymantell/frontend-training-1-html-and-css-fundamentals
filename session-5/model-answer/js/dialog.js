79913fd69b676b28cd1dc6d062236a6ea1f9fbe0
Andy Mantell <134642+andymantell@users.noreply.github.com>
2018-03-26 11:50:25 +0100

    Add a close button to the dialog element

--- session-5/model-answer/js/dialog.js
+++ session-5/model-answer/js/dialog.js
@@ -4,3 +4,9 @@ $('.wrapper').on('click', '.card > a', function (e) {
   var dialog = $(e.currentTarget).closest('.card').find('dialog')[0]
   dialog.showModal()
 })
+
+$('.wrapper').on('click', '.card__close', function (e) {
+  e.preventDefault()
+  var dialog = $(e.currentTarget).closest('.card').find('dialog')[0]
+  dialog.close()
+})
